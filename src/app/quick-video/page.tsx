1cb78c6205ef8dee005715df56973041e84171da
Devon Curry <labiffilmhouse@Devons-MacBook-Pro-2.local>
2026-02-11 10:40:32 -0500

    Update Amped Electric Loom video

--- src/app/quick-video/page.tsx
+++ src/app/quick-video/page.tsx
@@ -44,10 +44,8 @@ function QuickVideoContent() {
                     backgroundColor: '#f1f5f9'
                 }}>
                     <iframe
-                        src="https://www.loom.com/embed/a5189b6855d94053ac57ce296f42a514"
+                        src="https://www.loom.com/embed/9bbe7aa417d4499e807d00042a799c62"
                         frameBorder="0"
-                        webkitallowfullscreen="true"
-                        mozallowfullscreen="true"
                         allowFullScreen
                         style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
                     ></iframe>
